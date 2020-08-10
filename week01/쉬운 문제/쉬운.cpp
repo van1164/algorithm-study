b49f3144c24995ccdd53dab7965400253ea0fb19
van1164 <52437971+van1164@users.noreply.github.com>
2020-08-11 08:57:12 +0900

    wqe

--- week01/쉬운 문제/쉬운.cpp
+++ week01/쉬운 문제/쉬운.cpp
@@ -1,31 +1,14 @@
 #include<iostream>
-
+#include<cmath>
 using namespace std;
 
-char a[101];
 
 int main() {
-    while (cin>>a)
-    {
-        a = {};
-        int num[4] = { 0 };
-        for (int i = 0; a[i] != NULL; i++) {
-            if (a[i] == ' ') {
-                num[3]++;
-            }
-            else if (a[i] <= '9') {
-                num[2]++;
-            }
-            else if (a[i] <= 'Z') {
-                num[1]++;
-            }
-            else {
-                num[0]++;
-            }
-        }
-        for (int k = 0; k < 4; k++) {
-            cout << num[k] << " ";
-        }
-    }
+    int t;
+    int a[1010];
+    long float result;
+    long long int f;
+
+    cin >> t;
 
 }
