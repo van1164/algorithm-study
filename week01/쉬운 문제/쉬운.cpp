cf7ef1cd90b314926b331f71740b3aabefe8f77a
van133 <vkn123@naver.com>
2020-08-19 23:33:02 +0900

    DF

--- week01/쉬운 문제/쉬운.cpp
+++ week01/쉬운 문제/쉬운.cpp
@@ -5,30 +5,26 @@
 using namespace std;
 
 int main() {
+	int arr[3];
+	arr[0] = 1;
 	int t;
 	cin >> t;
-	int* arr;
-	arr = new int[t];
-	int a;
-	for (int i = 0; i < t; i++) {
-		cin >> a;
-		arr[i] = a;
+	int a, b;
+	int temp;
+	for (int i= 0; i < t; i++) {
+		cin >> a >> b;
+		temp = arr[a - 1];
+		arr[a - 1] = arr[b - 1];
+		arr[b - 1] = temp;
 	}
-	int x = t;
-	for (int i = t-1; i >=0; i--){
-		for (int j = 0; j < i; j++) {
-			if (arr[i] == -1) {
-				break;
-			}
-			if (arr[j] == -1) {
-				continue;
-			}
-			if (arr[i] >= arr[j]) {
-				arr[j] = -1;
-				x--;
-			}
-		}
+	if (arr[0] == 1) {
+		cout << 1;
 	}
-	cout << x;
+	else if (arr[1] == 1) {
+		cout << 2;
+	}
+	else {
+		cout << 3;
+	}
 }
 
